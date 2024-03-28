dc667bc9a093bfaa8dae69ad8bcef2dad3090b41
papy139 <pierreyves.vassard.pro@gmail.com>
2024-03-28 16:56:35 +0100

    decoy is out !

---
+++ include/shop-albertville-include.php
@@ -0,0 +1,13 @@
+  <header id="shop-presentation" class="px-32 pt-10">
+    <div class="background-background">
+      <img src="/asset/img/boutiques/boutique_albertville.jpg" class="img-shop" alt="" />
+      <div class="shop-info">
+        <h1 id="shop-title" class="mb-8 text-4xl">Boutique d'Albertville</h1>
+        <div id="shop-description" class="ml-9 text-2xl">
+          <p class="mb-3">Horaire: <span></span></p>
+          <p class="mb-3">Numéro de téléphone: <span></span></p>
+          <p class="mb-3">Localisation: <span></span></p>
+        </div>
+      </div>
+    </div>
+  </header>
